2fdccfca070776070e7cf3547b9dadf8ca21187d
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-10-17 14:38:26 +0300

    Update function

--- hw-7/script.js
+++ hw-7/script.js
@@ -8,7 +8,7 @@ let object = {};
 let objectWithoutPrototype = Object.create(null);
 let map = new Map();
 
-let key = 5; //10000; 10000000;
+let key = 100; //10000; 10000000;
 
 for (let i = 0; i < key; i++) {
   object[i] = i;
@@ -25,7 +25,7 @@ console.timeEnd('Adding to object');
 
 console.time('Adding to objectWithoutPrototype');
 objectWithoutPrototype[key] = key;
-console.timeEnd('Adding objectWithoutPrototype');
+console.timeEnd('Adding to objectWithoutPrototype');
 
 console.time('Adding to map');
 map.set(key);
@@ -64,6 +64,45 @@ console.timeEnd('Delete objectWithoutPrototype');
 console.time('Delete map');
 map.delete(key);
 console.timeEnd('Delete map');
+
+/**
+ * Object & map to array
+ */
+
+let result2;
+
+console.time('Object to array');
+result2 = Object.entries(object);
+console.log(result2);
+console.timeEnd('Object to array');
+
+console.time('objectWithoutPrototype to array');
+result2 = Object.entries(objectWithoutPrototype);
+console.log(result2);
+console.timeEnd('objectWithoutPrototype to array');
+
+console.time('Map to array');
+result2 = Array.from(map);
+console.log(result2);
+console.timeEnd('Map to array');
+
+/**
+ * Object & map to array with iteration
+ */
+
+let result3;
+
+console.time('Object to arr with iteration');
+Object.entries(object).forEach(([key, value]) => (result3 = value));
+console.timeEnd('Object to arr with iteration');
+
+console.time('objectWithoutPrototype to arr with iteration');
+Object.entries(objectWithoutPrototype).forEach(([key, value]) => (result3 = value));
+console.timeEnd('objectWithoutPrototype to arr with iteration');
+
+console.time('Map to arr with iteration');
+Array.from(map).forEach(([key, value]) => (result3 = value));
+console.timeEnd('Map to arr with iteration');
 
 //Написати функцію, яка приймає об’єкт у якості аргументу та повертає Map з тими самими даними.
 //Тобто просто перетворити об’єкт у Map.
